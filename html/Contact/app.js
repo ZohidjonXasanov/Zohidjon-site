341b4f7f932bb93ef1709ddab628f6bae945c57b
Zohidjon Xasanov <xzohidjon8348@gmail.com>
2023-08-23 11:19:35 +0500

    finsh from

--- html/Contact/app.js
+++ html/Contact/app.js
@@ -12,3 +12,23 @@ btn.addEventListener('click',function () {
     }
     return inp_name,inp_email,inp_tel,inp_msg.value = "";
 });
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+  
