fb7c1a9dc5029aa8fbb9ea8b60f49a9a6a869400
Zohidjon Xasanov <xzohidjon8348@gmail.com>
2023-09-30 21:03:24 +0500

    finsh

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var input_1 = document.getElementById("input_1");
 var input_2 = document.getElementById("input_2");
 var p = document.getElementById("p");
-var button_map = document.getElementById("button_map");
-var button_map_2 = document.getElementById("button_map_2");
 
 (function () {
         var txt;
@@ -15,17 +13,11 @@ var button_map_2 = document.getElementById("button_map_2");
 })();
 
 function btn() {
-    if (input_1.value == "xzohidjon8348@gmail.com") {
-        button_map_2.style.display = "none"
-        button_map.innerHTML = `<a class="button btn text-light" href="./index/index.html" onclick="btn()" role="button">Login</a>`
-    }
-    if (input_2.value == "Mullajonov") {
-        button_map_2.style.display = "none"
-        button_map.innerHTML = `<a class="button btn text-light" href="./index/index.html" onclick="btn()" role="button">Login</a>`
+    if (input_1.value == "xzohidjon8348@gmail.com" | input_2.value == "Mullajonov") {
+        window.location.replace(`./index/index.html`);
     }
     else {
-        button_map_2.style.display = "none"
-        p.innerHTML += "Ma'lumot noto'g'ri"
+        p.innerHTML = "Ma'lumot noto'g'ri"
     }
 }
 
